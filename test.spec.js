aaea4f4c9a304f95b657cc20be490f7babafa6d2
Greg <costagregorioalessio@gmail.com>
2020-08-22 09:29:46 +0100

    Create express hello world

---
+++ test.spec.js
@@ -0,0 +1,17 @@
+const server = require("./server");
+const request = require("supertest");
+
+describe("server", () => {
+  describe("GET /", () => {
+    it("returns Hello World", (done) => {
+      request(server)
+      .get('/')
+      .end((err, res) => {
+        if (err) { return done(err); }
+        expect(res.statusCode).toEqual(200);
+        expect(res.text).toEqual('Hello world!');
+        done();
+      });
+    });
+  });
+});
